2138b1e14683a40d8a7c6dc15aaab83cca9dd18a
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-16 21:35:32 +0900

    Create Account test in java folder closes #1

--- src/main/java/test/AccountTest.java
+++ src/main/java/test/AccountTest.java
@@ -2,5 +2,16 @@ package test;
 
 public class AccountTest {
 
+    public void testAccount() {
+        Account account = new Account();
 
+        if(account == null) {
+            throw new Exception("Failed to make account");
+        }
+    }
+
+    public static void main(String[] args) {
+        AccountTest accountTest = new AccountTest();
+        accountTest.testAccount();
+    }
 }
