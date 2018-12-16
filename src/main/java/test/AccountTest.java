66ece2f3792d183018ecec0bb16876ba53d28cc1
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-16 21:58:16 +0900

    Add JUnit, @Test to testAccount method Closes #2

--- src/main/java/test/AccountTest.java
+++ src/main/java/test/AccountTest.java
@@ -1,25 +1,16 @@
 package test;
 
 import main.Account;
+import org.junit.jupiter.api.Test;
 
 public class AccountTest {
 
+    @Test
     public void testAccount() throws Exception {
         Account account = new Account();
 
         if(account == null) {
             throw new Exception("Failed to make account");
         }
-    }
-
-    public static void main(String[] args) {
-        AccountTest accountTest = new AccountTest();
-        try {
-            accountTest.testAccount();
-        } catch (Exception e) {
-            System.out.println("Failed");
-            return;
-        }
-        System.out.println("Success!!");
     }
 }
