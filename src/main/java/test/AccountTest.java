779bb9ddac860caec2a871ebb65e9e0531e02d29
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-17 17:41:05 +0900

    Add test cases

--- src/main/java/test/AccountTest.java
+++ src/main/java/test/AccountTest.java
@@ -3,10 +3,31 @@ package test;
 import main.Account;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class AccountTest {
 
     @Test
     public void testAccount() throws Exception {
-        Account account = new Account();
+        Account account = new Account(100000);
+    }
+
+    @Test
+    public void testGetBalance() throws Exception {
+        Account account = new Account(100000);
+
+        if(account.getBalance() != 100000) {
+            fail();
+        }
+
+        account = new Account(1000);
+        if(account.getBalance() != 1000) {
+            fail("getBalance() => " + account.getBalance());
+        }
+
+        account = new Account(0);
+        if(account.getBalance() != 0) {
+            fail();
+        }
     }
 }
