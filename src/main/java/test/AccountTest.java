03d975462d4f89aed25c0f49d70aaa838b14dfa8
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-17 18:24:47 +0900

    Add BeforeAll annotation to test

--- src/main/java/test/AccountTest.java
+++ src/main/java/test/AccountTest.java
@@ -1,21 +1,26 @@
 package test;
 
 import main.Account;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTest {
+    private Account account;
+
+    @BeforeAll
+    private void setUp() {
+        account = new Account(10000);
+    }
 
     @Test
     public void testAccount() throws Exception {
-        Account account = new Account(100000);
     }
 
     @Test
     public void testGetBalance() throws Exception {
-        Account account = new Account(100000);
-        assertEquals(100000, account.getBalance(), "make account with 100000");
+        assertEquals(10000, account.getBalance(), "make account with 100000");
 
         account = new Account(100);
         assertEquals(100, account.getBalance(), "make account with 100");
@@ -23,14 +28,12 @@ public class AccountTest {
 
     @Test
     public void testWithdraw() throws Exception {
-        Account account = new Account(10000);
         account.withdraw(1000);
         assertEquals(9000, account.getBalance());
     }
 
     @Test
     public void testDeposit() throws Exception {
-        Account account = new Account(10000);
         account.deposit(1000);
         assertEquals(11000, account.getBalance());
     }
